3bc46c55d199b24682b15c8d45ecbea94bd817fa
andrew <wijayaandre769@gmail.com>
2025-09-08 06:19:34 +0700

    fear(next.config): add dynamic image source from cms (note: temp for current hosting)

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,63 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'picsum.photos',
+//         port: '',
+//         pathname: '/**',
+//       },
+//       // CMS API uploads
+//       {
+//         protocol: 'https',
+//         hostname: process.env.NEXT_PUBLIC_CMS_HOSTNAME || 'localhost',
+//         port: process.env.NEXT_PUBLIC_CMS_HOSTNAME ? '' : '1337',
+//         pathname: '/uploads/**',
+//       },
+//       // CMS Cloud Media CDN
+//       {
+//         protocol: 'https',
+//         hostname: process.env.NEXT_PUBLIC_CMS_MEDIA_HOSTNAME || 'localhost',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//     // Optimasi untuk performance
+//     formats: ['image/webp', 'image/avif'],
+//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+//     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+//     minimumCacheTTL: 60,
+//     dangerouslyAllowSVG: true,
+//     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+//   },
+//   // Add rewrites to proxy image requests to cms
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/uploads/:path*',
+//         destination: `${process.env.NEXT_PUBLIC_CMS_URL || 'http://localhost:1337'}/uploads/:path*`,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+
+// next.config.ts
 import type { NextConfig } from "next";
+
+const cmsUrlString = process.env.NEXT_PUBLIC_CMS_URL;
+
+if (!cmsUrlString) {
+  throw new Error("Environment variable NEXT_PUBLIC_CMS_URL is not set");
+}
+
+const cmsUrl = new URL(cmsUrlString);
+const cmsHostName = cmsUrl.hostname;
+const cmsMediaHostname = cmsHostName.replace('strapiapp.com', 'media.strapiapp.com');
 
 const nextConfig: NextConfig = {
   images: {
@@ -6,10 +65,26 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
+      // CMS API uploads
+      {
+        protocol: 'https',
+        hostname: cmsMediaHostname,
+      },
+      // CMS Cloud Media CDN
+      {
+        protocol: 'https',
+        hostname: cmsMediaHostname,
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      }
     ],
+    formats: ['image/webp', 'image/avif'],
+    minimumCacheTTL: 60,
   },
 };
 
